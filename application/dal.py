d1fd74bc54accbef7f1cf29e869561c1d5e95ef3
Sadot Arefin <arefin.sadot@gmail.com>
2022-12-25 10:38:07 +0600

    create function added

--- application/dal.py
+++ application/dal.py
@@ -5,10 +5,10 @@ from .models import *
 
 def add_user(email, password):
     user = User.query.filter_by(email=email).first()
-
+    result = False
     if not user:
         db.session.add(User(email=email, password=generate_password_hash(
                 password, method='sha256')))
         db.session.commit()
-    else:
-        print("user already exists")
+        result = True
+    return result
